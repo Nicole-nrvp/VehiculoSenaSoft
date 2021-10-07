124ad01011d0f363b8e2b790ff51c2f3fee9c175
Nicole-nrvp <82890414+Nicole-nrvp@users.noreply.github.com>
2021-10-06 22:16:27 -0500

    Conexion OKEY

---
+++ src/java/Util/Conexion.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import java.sql.*;
+
+/**
+ *
+ * @author Valentina
+ */
+public class Conexion {
+    
+    private String driver, user, password, nombrebd, urlbd;
+    private Connection conexion;
+    
+    
+    
+    public Conexion(){
+        
+        driver="com.mysql.jdbc.Driver";
+        user="root";
+        password="";
+        nombrebd="ventavehiculos";
+        urlbd="jdbc:mysql://localhost:3306/" + nombrebd;
+        
+        try {
+            Class.forName(driver).newInstance();
+            conexion = DriverManager.getConnection(urlbd, user, password);
+            System.out.println("Conexion Okey");
+            
+        } catch (Exception e) {
+            System.out.println("Error al conectarse" + e.toString());
+        }
+    }
+    
+    public Connection obtener(){
+        return conexion;
+    }
+    
+    public  Connection cerrar() throws SQLException{
+        conexion.close();
+        conexion = null;
+        return conexion;
+    }
+    
+    public static void main(String[] args) {
+        new Conexion();
+    }
+}
---
+++ web/Vistas/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : 6/10/2021, 09:57:56 PM
+    Author     : Valentina
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
